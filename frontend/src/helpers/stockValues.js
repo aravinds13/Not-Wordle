4d385baad8dc608bfe43ccc122eabed3a8d562d2
aravinds13 <aravindtvm13@gmail.com>
2025-03-17 03:54:27 -0400

    nginx update

---
+++ frontend/src/helpers/stockValues.js
@@ -0,0 +1,66 @@
+const stockValues = {
+  'defaultWordList': [
+    {
+      'isCurrentWord': true,
+      'word': '',
+      'status': ['black', 'black', 'black', 'black', 'black']
+    },
+    {
+      'isCurrentWord': false,
+      'word': '',
+      'status': ['black', 'black', 'black', 'black', 'black']
+    },
+    {
+      'isCurrentWord': false,
+      'word': '',
+      'status': ['black', 'black', 'black', 'black', 'black']
+    },
+    {
+      'isCurrentWord': false,
+      'word': '',
+      'status': ['black', 'black', 'black', 'black', 'black']
+    },
+    {
+      'isCurrentWord': false,
+      'word': '',
+      'status': ['black', 'black', 'black', 'black', 'black']
+    },
+    {
+      'isCurrentWord': false,
+      'word': '',
+      'status': ['black', 'black', 'black', 'black', 'black']
+    }
+  ],
+  'defaultCharStatus':{
+      'A': '#444',
+      'B': '#444',
+      'C': '#444',
+      'D': '#444',
+      'E': '#444',
+      'F': '#444',
+      'G': '#444',
+      'H': '#444',
+      'I': '#444',
+      'J': '#444',
+      'K': '#444',
+      'L': '#444',
+      'M': '#444',      
+      'N': '#444',
+      'O': '#444',
+      'P': '#444',
+      'Q': '#444',
+      'R': '#444',
+      'S': '#444',
+      'T': '#444',
+      'U': '#444',
+      'V': '#444',
+      'W': '#444',
+      'X': '#444',
+      'Y': '#444',
+      'Z': '#444',
+      'Enter': '#444',
+      '⌫': '#444',
+  }
+};
+
+export default stockValues;
